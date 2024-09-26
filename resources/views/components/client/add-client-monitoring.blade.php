55b357608c7e83a5e706c305ac9553248d12105b
ilhamariefprabowo <ilham@technosstudio.com>
2024-09-26 16:40:13 +0800

    Fix bot token

--- resources/views/components/client/add-client-monitoring.blade.php
+++ resources/views/components/client/add-client-monitoring.blade.php
@@ -1,97 +1,105 @@
   <!--begin::Modal - Add task-->
   <div class="modal fade" id="kt_modal_add_client" tabindex="-1" aria-hidden="true">
-    <!--begin::Modal dialog-->
-    <div class="modal-dialog modal-dialog-centered mw-650px">
-        <!--begin::Modal content-->
-        <div class="modal-content">
-            <!--begin::Modal header-->
-            <div class="modal-header" id="kt_modal_add_client_header">
-                <!--begin::Modal title-->
-                <h2 class="fw-bold">Add Client</h2>
-                <!--end::Modal title-->
-                <!--begin::Close-->
-                <div class="btn btn-icon btn-sm btn-active-icon-primary" data-bs-dismiss="modal">
-                    <i class="ki-duotone ki-cross fs-1">
-                        <span class="path1"></span>
-                        <span class="path2"></span>
-                    </i>
-                </div>
-                <!--end::Close-->
-            </div>
-            <!--end::Modal header-->
-            <!--begin::Modal body-->
-            <div class="modal-body px-5 my-7">
-                <!--begin::Form-->
-                <form id="kt_modal_add_client_form" class="form" action="#">
-                    <!--begin::Scroll-->
-                    <div class="d-flex flex-column scroll-y px-5 px-lg-10" id="kt_modal_add_client_scroll" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_add_client_header" data-kt-scroll-wrappers="#kt_modal_add_client_scroll" data-kt-scroll-offset="300px">
-                       <!--begin::Input group-->
-                    <div class="fv-row mb-7">
-                        <!--begin::Label-->
-                        <label class="fs-6 fw-semibold form-label mb-2">
-                            <span class="required">Nama Clien Anda</span>
-                        </label>
-                        <!--end::Label-->
-                        <!--begin::Input-->
-                        <input type="text" class="form-control form-control-solid" placeholder="Masukan Nama Client Anda" id="nameadd" name="name" />
-                        <!--end::Input-->
-                    </div>
-                    <!--end::Input group-->
-                       <!--begin::Input group-->
-                    <div class="fv-row mb-7">
-                        <!--begin::Label-->
-                        <label class="fs-6 fw-semibold form-label mb-2">
-                            <span>Deskripsi</span>
-                        </label>
-                        <!--end::Label-->
-                        <!--begin::Input-->
-                        <input type="text" class="form-control form-control-solid" placeholder="Masukan Deskripsi" name="description" />
-                        <!--end::Input-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="fv-row mb-7">
-                        <!--begin::Label-->
-                        <label class="fs-6 fw-semibold form-label mb-2">
-                            <span class="required">Bot Telegram</span>
-                        </label>
-                        <!--end::Label-->
-                        <!--begin::Input-->
-                        <input type="text" class="form-control form-control-solid" placeholder="Masukan Bot Token Client Anda" name="bot_token" />
-                        <!--end::Input-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="fv-row mb-7">
-                        <!--begin::Label-->
-                        <label class="fs-6 fw-semibold form-label mb-2">
-                            <span class="required">Chat Id</span>
-                        </label>
-                        <!--end::Label-->
-                        <!--begin::Input-->
-                        <input type="text" class="form-control form-control-solid" placeholder="Masukan Chat ID Clien Anda" name="chat_id" />
-                        <!--end::Input-->
-                    </div>
-                    <!--end::Input group-->
-                    </div>
-                    <!--end::Scroll-->
-                    <!--begin::Actions-->
-                    <div class="text-center pt-10">
-                        <button type="reset" class="btn btn-light me-3" data-bs-dismiss="modal" data-kt-customer-modal-action="cancel">Discard</button>
-                        <button type="submit" class="btn btn-primary" data-kt-clientmonitoring-modal-action="submit">
-                            <span class="indicator-label">Submit</span>
-                            <span class="indicator-progress">Please wait...
-                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                        </button>
-                    </div>
-                    <!--end::Actions-->
-                </form>
-                <!--end::Form-->
-            </div>
-            <!--end::Modal body-->
-        </div>
-        <!--end::Modal content-->
-    </div>
-    <!--end::Modal dialog-->
-</div>
-<!--end::Modal - Add task-->
+      <!--begin::Modal dialog-->
+      <div class="modal-dialog modal-dialog-centered mw-650px">
+          <!--begin::Modal content-->
+          <div class="modal-content">
+              <!--begin::Modal header-->
+              <div class="modal-header" id="kt_modal_add_client_header">
+                  <!--begin::Modal title-->
+                  <h2 class="fw-bold">Add Client</h2>
+                  <!--end::Modal title-->
+                  <!--begin::Close-->
+                  <div class="btn btn-icon btn-sm btn-active-icon-primary" data-bs-dismiss="modal">
+                      <i class="ki-duotone ki-cross fs-1">
+                          <span class="path1"></span>
+                          <span class="path2"></span>
+                      </i>
+                  </div>
+                  <!--end::Close-->
+              </div>
+              <!--end::Modal header-->
+              <!--begin::Modal body-->
+              <div class="modal-body px-5 my-7">
+                  <!--begin::Form-->
+                  <form id="kt_modal_add_client_form" class="form" action="#">
+                      <!--begin::Scroll-->
+                      <div class="d-flex flex-column scroll-y px-5 px-lg-10" id="kt_modal_add_client_scroll"
+                          data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-max-height="auto"
+                          data-kt-scroll-dependencies="#kt_modal_add_client_header"
+                          data-kt-scroll-wrappers="#kt_modal_add_client_scroll" data-kt-scroll-offset="300px">
+                          <!--begin::Input group-->
+                          <div class="fv-row mb-7">
+                              <!--begin::Label-->
+                              <label class="fs-6 fw-semibold form-label mb-2">
+                                  <span class="required">Nama Clien Anda</span>
+                              </label>
+                              <!--end::Label-->
+                              <!--begin::Input-->
+                              <input type="text" class="form-control form-control-solid"
+                                  placeholder="Masukan Nama Client Anda" id="nameadd" name="name" />
+                              <!--end::Input-->
+                          </div>
+                          <!--end::Input group-->
+                          <!--begin::Input group-->
+                          <div class="fv-row mb-7">
+                              <!--begin::Label-->
+                              <label class="fs-6 fw-semibold form-label mb-2">
+                                  <span>Deskripsi</span>
+                              </label>
+                              <!--end::Label-->
+                              <!--begin::Input-->
+                              <input type="text" class="form-control form-control-solid"
+                                  placeholder="Masukan Deskripsi" name="description" />
+                              <!--end::Input-->
+                          </div>
+                          <!--end::Input group-->
+                          <!--begin::Input group-->
+                          <div class="fv-row mb-7">
+                              <!--begin::Label-->
+                              <label class="fs-6 fw-semibold form-label mb-2">
+                                  <span class="required">Bot Telegram</span>
+                              </label>
+                              <!--end::Label-->
+                              <!--begin::Input-->
+                              <input type="text" class="form-control form-control-solid"
+                                  placeholder="Masukan Bot Token Client Anda" name="bot_token" />
+                              <!--end::Input-->
+                          </div>
+                          <!--end::Input group-->
+                          <!--begin::Input group-->
+                          <div class="fv-row mb-7">
+                              <!--begin::Label-->
+                              <label class="fs-6 fw-semibold form-label mb-2">
+                                  <span class="required">Chat Id</span>
+                              </label>
+                              <!--end::Label-->
+                              <!--begin::Input-->
+                              <input type="text" class="form-control form-control-solid"
+                                  placeholder="Masukan Chat ID Clien Anda" name="chat_id" />
+                              <!--end::Input-->
+                          </div>
+                          <!--end::Input group-->
+                      </div>
+                      <!--end::Scroll-->
+                      <!--begin::Actions-->
+                      <div class="text-center pt-10">
+                          <button type="reset" class="btn btn-light me-3" data-bs-dismiss="modal"
+                              data-kt-customer-modal-action="cancel">Discard</button>
+                          <button type="submit" class="btn btn-primary" data-kt-clientmonitoring-modal-action="submit">
+                              <span class="indicator-label">Submit</span>
+                              <span class="indicator-progress">Please wait...
+                                  <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                          </button>
+                      </div>
+                      <!--end::Actions-->
+                  </form>
+                  <!--end::Form-->
+              </div>
+              <!--end::Modal body-->
+          </div>
+          <!--end::Modal content-->
+      </div>
+      <!--end::Modal dialog-->
+  </div>
+  <!--end::Modal - Add task-->
--- resources/views/monitoringweb/client/list.blade.php
+++ resources/views/monitoringweb/client/list.blade.php
@@ -1,144 +1,156 @@
 @extends('layout.app')
 @section('content')
-<!--begin::Content wrapper-->
-<div class="d-flex flex-column flex-column-fluid">
-    <!--begin::Toolbar-->
-    <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
-        <!--begin::Toolbar container-->
-        <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
-            <!--begin::Page title-->
-            <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
-                <!--begin::Title-->
-                <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">Client List</h1>
-                <!--end::Title-->
-                <!--begin::Breadcrumb-->
-                <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-muted">
-                        <a href="index.html" class="text-muted text-hover-primary">Home</a>
-                    </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item">
-                        <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                    </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-muted">User Management</li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item">
-                        <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                    </li>
-                    <!--end::Item-->
-                    <!--begin::Item-->
-                    <li class="breadcrumb-item text-muted">Users</li>
-                    <!--end::Item-->
-                </ul>
-                <!--end::Breadcrumb-->
-            </div>
-            <!--end::Page title-->
-        </div>
-        <!--end::Toolbar container-->
-    </div>
-    <!--end::Toolbar-->
-    <!--begin::Content-->
-    <div id="kt_app_content" class="app-content flex-column-fluid">
-        <!--begin::Content container-->
-        <div id="kt_app_content_container" class="app-container container-xxl">
-            <!--begin::Card-->
-            <div class="card">
-                <!--begin::Card header-->
-                <div class="card-header border-0 pt-6">
-                    <!--begin::Card title-->
-                    <div class="card-title">
-                        <!--begin::Search-->
-                        <div class="d-flex align-items-center position-relative my-1">
-                            <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-5">
-                                <span class="path1"></span>
-                                <span class="path2"></span>
-                            </i>
-                            <input type="text" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Search user" />
-                        </div>
-                        <!--end::Search-->
-                    </div>
-                    <!--begin::Card title-->
-                    <!--begin::Card toolbar-->
-                    <div class="card-toolbar">
-                        <!--begin::Toolbar-->
-                        <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
-                            <!--begin::Add user-->
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_client">
-                            <i class="ki-duotone ki-plus fs-2"></i>Add Client</button>
-                            <!--end::Add user-->
-                        </div>
-                        <!--end::Toolbar-->
-                        <!--begin::Group actions-->
-                        <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
-                            <div class="fw-bold me-5">
-                            <span class="me-2" data-kt-user-table-select="selected_count"></span>Selected</div>
-                            <button type="button" class="btn btn-danger" data-kt-user-table-select="delete_selected">Delete Selected</button>
-                        </div>
-                        <!--end::Group actions-->
-                    </div>
-                    <!--end::Card toolbar-->
+    <!--begin::Content wrapper-->
+    <div class="d-flex flex-column flex-column-fluid">
+        <!--begin::Toolbar-->
+        <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
+            <!--begin::Toolbar container-->
+            <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
+                <!--begin::Page title-->
+                <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+                    <!--begin::Title-->
+                    <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">Client
+                        List</h1>
+                    <!--end::Title-->
+                    <!--begin::Breadcrumb-->
+                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">
+                            <a href="index.html" class="text-muted text-hover-primary">Home</a>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">User Management</li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">Users</li>
+                        <!--end::Item-->
+                    </ul>
+                    <!--end::Breadcrumb-->
                 </div>
-                <!--end::Card header-->
-                <!--begin::Card body-->
-                <div class="card-body py-4">
-                    <!--begin::Table-->
-                    <table class="table align-middle table-row-dashed fs-6 gy-5 data-tableclientview">
-                        <thead>
-                            <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
-                                <th class="min-w-125px">Client Name</th>
-                                <th class="min-w-250px">Bot Token</th>
-                                <th class="min-w-125px">Chat Id</th>
-                                <th class="text-end min-w-100px">Actions</th>
-                            </tr>
-                        </thead>
+                <!--end::Page title-->
+            </div>
+            <!--end::Toolbar container-->
+        </div>
+        <!--end::Toolbar-->
+        <!--begin::Content-->
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <!--begin::Content container-->
+            <div id="kt_app_content_container" class="app-container container-xxl">
+                <!--begin::Card-->
+                <div class="card">
+                    <!--begin::Card header-->
+                    <div class="card-header border-0 pt-6">
+                        <!--begin::Card title-->
+                        <div class="card-title">
+                            <!--begin::Search-->
+                            <div class="d-flex align-items-center position-relative my-1">
+                                <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-5">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                                <input type="text" data-kt-user-table-filter="search"
+                                    class="form-control form-control-solid w-250px ps-13" placeholder="Search user" />
+                            </div>
+                            <!--end::Search-->
+                        </div>
+                        <!--begin::Card title-->
+                        <!--begin::Card toolbar-->
+                        <div class="card-toolbar">
+                            <!--begin::Toolbar-->
+                            <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
+                                <!--begin::Add user-->
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#kt_modal_add_client">
+                                    <i class="ki-duotone ki-plus fs-2"></i>Add Client</button>
+                                <!--end::Add user-->
+                            </div>
+                            <!--end::Toolbar-->
+                            <!--begin::Group actions-->
+                            <div class="d-flex justify-content-end align-items-center d-none"
+                                data-kt-user-table-toolbar="selected">
+                                <div class="fw-bold me-5">
+                                    <span class="me-2" data-kt-user-table-select="selected_count"></span>Selected
+                                </div>
+                                <button type="button" class="btn btn-danger"
+                                    data-kt-user-table-select="delete_selected">Delete Selected</button>
+                            </div>
+                            <!--end::Group actions-->
+                        </div>
+                        <!--end::Card toolbar-->
+                    </div>
+                    <!--end::Card header-->
+                    <!--begin::Card body-->
+                    <div class="card-body py-4">
+                        <!--begin::Table-->
+                        <table class="table align-middle table-row-dashed fs-6 gy-5 data-tableclientview">
+                            <thead>
+                                <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                                    <th class="min-w-125px">Client Name</th>
+                                    <th class="min-w-250px">Bot Token</th>
+                                    <th class="min-w-125px">Chat Id</th>
+                                    <th class="text-end min-w-100px">Actions</th>
+                                </tr>
+                            </thead>
 
-                    </table>
-                    <!--end::Table-->
+                        </table>
+                        <!--end::Table-->
+                    </div>
+                    <!--end::Card body-->
                 </div>
-                <!--end::Card body-->
+                <!--end::Card-->
+                @include('components.client.add-client-monitoring')
+                @include('components.client.edit-client-monitoring')
             </div>
-            <!--end::Card-->
-            @include('components.client.add-client-monitoring')
-            @include('components.client.edit-client-monitoring')
+            <!--end::Content container-->
         </div>
-        <!--end::Content container-->
+        <!--end::Content-->
     </div>
-    <!--end::Content-->
-</div>
-<!--end::Content wrapper-->
+    <!--end::Content wrapper-->
 @endsection
 
 @push('scripts')
-<script type="text/javascript">
-    $(function () {
+    <script type="text/javascript">
+        $(function() {
 
-      var table = $('.data-tableclientview').DataTable({
-          processing: true,
-          serverSide: true,
-          ajax: "{{ route('clientmonitoringweb.index') }}",
-          columns: [
-            //   {data: 'id'},
-              {data: 'name'},
-              {data: 'chat_id'},
-              {data: 'bot_token'},
-              {
-                  data: 'action',
-                  name: 'action',
-                  orderable: true,
-                  searchable: true
-              },
-            //   {data: 'action', name: 'action', orderable: false, searchable: false},
-          ]
-      });
+            var table = $('.data-tableclientview').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('clientmonitoringweb.index') }}",
+                columns: [
+                    //   {data: 'id'},
+                    {
+                        data: 'name'
+                    },
+                    {
+                        data: 'bot_token'
+                    },
+                    {
+                        data: 'chat_id'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: true,
+                        searchable: true
+                    },
+                    //   {data: 'action', name: 'action', orderable: false, searchable: false},
+                ]
+            });
 
-    });
-  </script>
-<script src="{{ asset('assets/js/custom/apps/client-monitoring-web/add.js') }}"></script>
-<script src="{{ asset('assets/js/custom/apps/client-monitoring-web/edit.js') }}"></script>
-<script src="{{ asset('assets/js/custom/apps/client-monitoring-web/delete.js') }}"></script>
+        });
+    </script>
+    <script src="{{ asset('assets/js/custom/apps/client-monitoring-web/add.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/apps/client-monitoring-web/edit.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/apps/client-monitoring-web/delete.js') }}"></script>
 @endpush
